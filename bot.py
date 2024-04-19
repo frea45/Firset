1911a2cbc355da47ca0851d5816d807d36511edc
frea45 <110092585+frea45@users.noreply.github.com>
2024-04-19 14:47:10 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+from pyrogram import client , filters
+from pyrogram.types import Message 
+
+API_ID = 3335796
+API_HASH = "138b992a0e672e8346d8439c3f42ea78"
+
+
+app = client("bot",
+API_ID,
+API_HASH,
+BOT_TOKEN= "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0")
+
+@app.on.message(filters.text)
+def test (c:client = m:message):
+  chatid = m.chat.id
+  app.send.message(chatid , "یا خدا")
+  
+  app.run()
