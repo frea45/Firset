9a59afdc24fda7910d9e73bbd74f32985bfe5bfa
frea45 <110092585+frea45@users.noreply.github.com>
2024-04-20 20:34:29 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from pyrogram import client , filters
-from pyrogram.types import Message , ReplyKeyboardMarkup
+from pyrogram.types import Message 
 import asyncio
 
 
-API_ID = 3335796
+API_ID = "3335796"
 API_HASH = "138b992a0e672e8346d8439c3f42ea78"
 
 
@@ -12,23 +12,12 @@ API_ID,
 API_HASH,
 BOT_TOKEN= "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0")
 
-@app.on.message(filters.private , group=0) 
-  async def test (client = message):
-  text = message.text
-  chat = message.chat.id
-  if text == "/start":
-    await app.send_message (
-      chat , "welcome",
-      reply_markup=ReplyKeyboardMarkup(
-        [
-          ["sallam"]
-        ],
-        
-         resize_keyboard = True 
-        
-        ) 
-      )
-    
+
+@AHBot.on_message(filters.command(["reset"]) & filters.private)
+async def reset(bot, update):
+        await db.delete_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
+        await update.reply_text("Settings reseted successfully"
     
  
   
