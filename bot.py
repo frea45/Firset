39b4349dd28c7e5627c392f381142368eb64f6fe
frea45 <110092585+frea45@users.noreply.github.com>
2024-04-20 20:20:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from pyrogram import client , filters
-from pyrogram.types import Message 
+from pyrogram.types import Message , ReplyKeyboardMarkup
+import asyncio
 
-API_ID = "3335796"
+
+API_ID = 3335796
 API_HASH = "138b992a0e672e8346d8439c3f42ea78"
 
 
@@ -10,9 +12,24 @@ API_ID,
 API_HASH,
 BOT_TOKEN= "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0")
 
-@app.on.message(filters.text)
-def test (c:client = m:message):
-  chatid = m.chat.id
-  app.send.message(chatid , "یا خدا")
+@app.on.message(filters.private , group=0) 
+  async def test (client = message):
+  text = message.text
+  chat = message.chat.id
+  if text == "/start":
+    await app.send_message (
+      chat , "welcome",
+      reply_markup=ReplyKeyboardMarkup(
+        [
+          ["sallam"]
+        ],
+        
+         resize_keyboard = True 
+        
+        ) 
+      )
+    
+    
+ 
   
   app.run()
