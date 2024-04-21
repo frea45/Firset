cd46a6e7cd54fbbaf80863c7bf8f7abc709bddaa
frea45 <110092585+frea45@users.noreply.github.com>
2024-04-21 12:55:11 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,29 @@
-from pyrogram import client , filters
-from pyrogram.types import Message 
-import asyncio
+from pyrogram import Client , filters
 
 
-API_ID = "3335796"
-API_HASH = "138b992a0e672e8346d8439c3f42ea78"
+
+api_id = 3335796
+api_hash = '138b992a0e672e8346d8439c3f42ea78'
+token = '5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0'
+
+app = Client(
+  "uploader",
+api_id = api_id,
+api_hash = api_hash,
+bot_token = token
+)
+
+import logging
+logging.basicConfig(
+format = '%(asctime)s – %(name)s – %(levelname)s – %(message)s', 
+level = logging.INFO
+)
+LOGGER = logging.getLoggor(__name__)
 
 
-app = client("bot",
-API_ID,
-API_HASH,
-BOT_TOKEN= "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0")
-
-
-@app.on_message(filters.command(["reset"]) & filters.private)
-async def reset(bot, update):
-        await db.delete_user(update.from_user.id)
-        await db.add_user(update.from_user.id)
-        await update.reply_text("Settings reseted successfully"
-    
- 
-  
+@app.on.message(filters.private)
+ def hello (client, message):
+     message.replay("hello")
+     
+     
   app.run()
