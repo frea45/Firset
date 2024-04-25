b1ab7785ac85a495878ba63f67a54702b0fcfe1b
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-25 23:32:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-from pyrogram import Client , filters
+from pyrogram import Client 
 import logging
-
+from pyrogram.handlers import MessageHandler
+from pyrogram.errors import BadRequest, Forbidden, ...
 
 api_id = 3335796
-api_hash = '138b992a0e672e8346d8439c3f42ea78'
-bot_token = '5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0'
+api_hash = "138b992a0e672e8346d8439c3f42ea78"
+bot_token = "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0"
 
 
 app = Client("uploader",api_id=api_id, api_hash=api_hash, bot_token=bot_token )
@@ -17,14 +18,4 @@ level = logging.INFO
 LOGGER = logging.getLoggor(__name__)
 
 
-#@app.on.message(filters.private)
-# def hello (client, message):
-  #   message.replay("hello")
-
-
-
-async def main():
-    async with app:
-        await app.send_message("me", "Hi!")
-
-app.run(main())
+app.run()
