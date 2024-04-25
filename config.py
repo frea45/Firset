b210cb429f024c60b6ac8dd1e087269e475c4ba6
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-26 01:06:21 +0330

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ api_hash = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
 
 bot_token = os.environ.get("BOT_TOKEN", "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0") 
 
-PORT = os.environ.get('PORT', '8080')
+#PORT = os.environ.get('PORT', '8080')
 
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
