4ccc369dbed685f5c4173a9f90f4fae27f2f2878
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-26 15:45:26 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 import logging
 import config
+import os
+import traceback
 #import API_ID, API_HASH, BOT_TOKEN 
 from pyrogram.handlers import MessageHandler
 #from pyrogram.errors import BadRequest, Forbidden, ...
@@ -13,17 +15,15 @@ bot_token = "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0"
 app = Client("uploader",api_id=api_id, api_hash=api_hash, bot_token=bot_token )
 
 
-@app.on_message(filters.text & filters.private)
-async def echo(client, message):
+#@app.on_message(filters.text & filters.private)
+#async def echo(client, message):
   #  await message.reply(message.text)
-    if message.text :
-      if message.text == '/start':
-      app.send_message(
-        message.chat_id, 'Hiii',
-        reply_to_message_id=message.id 
-                      )
-   elif message.photo :
-     pass 
+    
+
+@app.on_message(filters.private & filters.command("help"))
+async def clear_user_batch(Client, Message):
+   # MediaList[f"{str(m.from_user.id)}"] = []
+    await m.reply_text("Cleared your batch files successfully!")
 logging.info('startter')
 
 
