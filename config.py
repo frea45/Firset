f59cb6fb8413ebf105b1529c56e16d1da135ba1e
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-29 15:11:17 +0330

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,9 @@ bot_token = os.environ.get("BOT_TOKEN", "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-
 BUN_VERSION = os.environ.get("BUN_VERSION", "1.1.0")
 
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+
+
+
+WELCOME_MESSAGE = "Hi welcome bot"
+
+GROUP = "tested"
