c206405d1a460d8dfc2b325d9d72978785b53f53
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-29 15:12:19 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-api_id = os.environ.get("API_ID", "3335796")
+api_id = os.environ.get("api_id", "3335796")
 
-api_hash = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
+api_hash = os.environ.get("api_hash", "138b992a0e672e8346d8439c3f42ea78")
 
-bot_token = os.environ.get("BOT_TOKEN", "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0") 
+bot_token = os.environ.get("bot_token", "5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0") 
 
 #PORT = os.environ.get('PORT', '8080')
 BUN_VERSION = os.environ.get("BUN_VERSION", "1.1.0")
