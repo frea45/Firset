f04df6fc04398626d36d7743afc148cd75fd9cdb
frea45 <110092585+frea45@users.noreply.github.com>
2024-07-19 14:08:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,17 @@ api_id = 3335796,
 api_hash = "138b992a0e672e8346d8439c3f42ea78",
 bot_token = "7439915063:AAGWKexBKxqCiQXIWSpLmHZTqpG8gQ-1RJA"
 
-fars = Client("test", api_id = api_id, api_hash = api_hash, bot_token = bot_token )
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
 
 
+@app.on_message()
+async def my_handler(client, message):
+    await message.forward("me")
+    print ("working")
 
-@fars.on_message(filters.command("start"))
-  def welcome(client, message):
 
-print ("")
-
-
-fars.run()
+app.run()
